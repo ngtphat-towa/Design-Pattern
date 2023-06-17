9b9c9e873f64c7bd2c12f347d1905339c7f65328
ElHanaya <89678375+phatnguyen1710@users.noreply.github.com>
2023-06-17 08:35:37 +0700

    Chapter_09_ITERATOR Java Imp Cafe Diner

---
+++ Chapter_09_ITERATOR_COMPOSITE/ITERATOR/CafeDinerMerger/Iteractors/AlternatingDinerMenuIterator.java
@@ -0,0 +1,30 @@
+package Chapter_09_ITERATOR_COMPOSITE.ITERATOR.CafeDinerMerger.Iteractors;
+
+import java.util.Calendar;
+import java.util.Iterator;
+
+import Chapter_09_ITERATOR_COMPOSITE.ITERATOR.CafeDinerMerger.Menu.MenuItem;
+
+public class AlternatingDinerMenuIterator implements Iterator<MenuItem> {
+    MenuItem[] items;
+    int position;
+
+    public AlternatingDinerMenuIterator(MenuItem[] items) {
+        this.items = items;
+        position = Calendar.DAY_OF_WEEK % 2;
+    }
+
+    public MenuItem next() {
+        MenuItem menuItem = items[position];
+        position = position + 2;
+        return menuItem;
+    }
+
+    public boolean hasNext() {
+        if (position >= items.length || items[position] == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
